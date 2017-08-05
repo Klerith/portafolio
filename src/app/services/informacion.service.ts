b64176692f5ddce305bc09ad2bf78fc22b1162a6
Fernando Herrera <fernando.herrera85@gmail.com>
2017-08-04 18:34:02 -0600

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,15 +7,33 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
 
   constructor( public http:Http ) {
 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+
+  }
+
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
               .subscribe( data =>{
-                  console.log(data.json());
+                  // console.log(data.json());
                   this.cargada = true;
                   this.info = data.json();
-              })
+              });
+  }
+
+  public carga_sobre_nosotros(){
+
+    this.http.get("https://paginaweb-23ef1.firebaseio.com/equipo.json")
+              .subscribe( data =>{
+                  console.log(data.json());
+                  this.cargada_sobre_nosotros = true;
+                  this.equipo = data.json();
+              });
 
   }
 
