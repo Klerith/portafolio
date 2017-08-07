7562c07717c38ddcc481c4b91e86b76e279a852d
Fernando Herrera <fernando.herrera85@gmail.com>
2017-08-07 16:07:07 -0600

    Terminamos los fixes de la version 1

--- src/app/components/portafolio/portafolio.component.ts
+++ src/app/components/portafolio/portafolio.component.ts
@@ -7,8 +7,8 @@ import { ProductosService } from "../../services/productos.service";
 })
 export class PortafolioComponent {
 
-  constructor( private _ps:ProductosService ){
-    
+  constructor( public _ps:ProductosService ){
+
   }
 
 }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -11,7 +11,7 @@ export class SearchComponent{
   termino:string = undefined;
 
   constructor( private route:ActivatedRoute,
-               private _ps:ProductosService) {
+               public _ps:ProductosService) {
 
     route.params.subscribe( parametros=>{
 
